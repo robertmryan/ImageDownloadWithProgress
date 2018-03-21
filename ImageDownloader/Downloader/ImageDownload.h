ed469f3641fea2209849b39493552182ec09b103
Robert M. Ryan <robert.ryan@mindspring.com>
2018-03-21 16:21:30 -0700

    Update ImageDownloader
    Pull complicated logic out of the delegate method.

--- ImageDownloader/Downloader/ImageDownload.h
+++ ImageDownloader/Downloader/ImageDownload.h
@@ -21,6 +21,14 @@ NS_ASSUME_NONNULL_BEGIN
          filename:(NSString * _Nullable)filename;
 
 /**
+ Update NSProgress
+ @param totalBytesWritten Total number of bytes received thus far.
+ @param totalBytesExpectedToWrite Total number of bytes expected (may be -1 if unknown).
+ */
+
+- (void)updateProgressForTotalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite;
+
+/**
  Start download of image.
 
  @param session The NSURLSession upon which this download will be processed.
